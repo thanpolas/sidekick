1e063ecc7c936f5f2fa06c8bfd65b026400de9b2
Thanos Polychronakis <thanpolas@gmail.com>
2024-12-22 12:01:03 +0200

    more helpers

--- src/format.js
+++ src/format.js
@@ -16,6 +16,19 @@ exports.getPercentHr = (a, b, decimals = 2) => {
 };
 
 /**
+ * Get difference expressed in percentage of fraction into human readable format.
+ *
+ * @param {number} a Target.
+ * @param {number} b Compare.
+ * @param {number=} decimals How many decimals to have.
+ * @return {string} Human readable percentage.
+ */
+exports.getPercentDiffHr = (a, b, decimals = 2) => {
+  const percent = b / a - 1;
+  return `${(percent * 100).toFixed(decimals)}%`;
+};
+
+/**
  * Shorten an ethereum address as "0x123..98765".
  *
  * @param {string} address An ethereum address.
@@ -34,4 +47,20 @@ exports.shortAddress = (address) => {
 exports.formatNumber = (num) => {
   const intl = new Intl.NumberFormat('en-US');
   return intl.format(num);
+};
+
+/**
+ * Local helper to format currency.
+ *
+ * @param {number|string} num The number to format.
+ * @param {number=} decimals How many decimals to show.
+ * @return {string}
+ */
+exports.formatCurrency = (num, decimals = 0) => {
+  return new Intl.NumberFormat('en', {
+    style: 'currency',
+    currency: 'USD',
+    unitDisplay: 'short',
+    maximumFractionDigits: decimals,
+  }).format(num);
 };
